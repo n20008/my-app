6c9e0095bbfa7ea095f97c47e93fc4272c31567c
Komei Kuninaka <n20008@std.it-college.ac.jp>
2021-02-15 06:12:08 +0000

    ok

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+
+function App () {
+  const [animeList, SetAnimeList] = useState([])
+  const [search, SetSearch] = useState('')
+
+  const GetTopAnime = async () => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/top/anime/1/bypopularity`
+    ).then(res => res.json())
+  }
+
+  const HandleSearch = e => {
+    e.preventDefault()
+
+    FetchAnime(search)
+  }
+
+  const FetchAnime = async query => {
+    const temp = await fetch(
+      `https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`
+    ).then(res => res.json())
+
+    SetAnimeList(temp.results)
+  }
+
+  useEffect(() => {
+    GetTopAnime()
+  }, [])
+
+  return (
+    <div className='App'>
+      <Header />
+      <div className='content-wrap'>
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          SetSearch={SetSearch}
+          animeList={animeList}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
